82c727db8597b4a2e54543878fd5ed9baa2d9628
Vidur Kothur <vidurkothur1028@gmail.com>
2025-09-27 16:21:07 -0400

    feat: added clap in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 mod file_tools;
 mod zip_tools;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    zip: String,
+
+    #[arg(short, long)]
+    repo: String,
+
+    #[arg(short, long, required = true, num_args = 0..)]
+    usernames: Vec<String>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{}", args.zip);
+    println!("{}", args.repo);
+    for u in args.usernames {
+        println!("{}", u);
+    }
 }
