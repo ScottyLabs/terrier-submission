a02e41a7b161afc3395b8da67ffddb8f4c014fb2
SnowballSH <derek070211@gmail.com>
2025-10-04 16:55:51 -0400

    feat: use serde parse feature

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,18 @@ mod file_tools;
 mod git_tools;
 mod zip_tools;
 
-use chrono::{DateTime, Utc};
 use clap::Parser;
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
 use std::vec::Vec;
 
-#[derive(Debug)]
-struct Data {
+#[derive(Debug, Serialize, Deserialize)]
+struct ConfigData {
     zip: String,
     repo: String,
     usernames: Vec<String>,
-    start_time: DateTime<Utc>,
+    start_time: u64,
 }
 
 #[derive(Parser, Debug)]
@@ -26,39 +24,13 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let mut file = File::open(args.path).map_err(|_e| "JSON file path not found.".to_string())?;
+    let mut file =
+        File::open(&args.path).map_err(|_e| format!("JSON file {} does not exist.", args.path))?;
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let serde_data: Value = serde_json::from_str(&contents)?;
-
-    let serde_path = serde_data["zip"].as_str().unwrap().to_string();
-
-    let path = Path::new(&serde_path);
-    if !path.exists() {
-        return Err("Zip path provided does not exist.".into());
-    }
-
-    let serde_usernames = serde_data["usernames"]
-        .as_array()
-        .unwrap()
-        .iter()
-        .map(|v| v.as_str().unwrap().to_string())
-        .collect();
-
-    let start_time_str = serde_data["start_time"].as_str().unwrap();
-
-    let serde_date: DateTime<Utc> = start_time_str
-        .parse()
-        .map_err(|_| "Failed to parse start time string")?;
-
-    let data = Data {
-        zip: serde_path,
-        repo: serde_data["repo"].as_str().unwrap().to_string(),
-        usernames: serde_usernames,
-        start_time: serde_date,
-    };
+    let data: ConfigData = serde_json::from_str(&contents)?;
 
     println!("{:?}", &data);
 
