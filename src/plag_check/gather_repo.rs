2128e1394bc7b1416307b3688f7058309223a69e
Vidur Kothur <vidurkothur1028@gmail.com>
2025-11-15 17:05:31 -0500

    feat: added size checking to gather_repo_urls_from_user and clone_repos_into_dir

--- src/plag_check/gather_repo.rs
+++ src/plag_check/gather_repo.rs
@@ -2,18 +2,19 @@ use crate::git_tools::repository::GithubRepo;
 use octocrab::Octocrab;
 use std::path::PathBuf;
 
-async fn gather_repo_urls_from_user(
+async fn gather_repo_urls_and_sizes_from_user(
     octocrab: Octocrab,
     username: &str,
-) -> octocrab::Result<Vec<String>> {
+) -> octocrab::Result<Vec<(String, u32)>> {
     let repos_page = octocrab.users(username).repos().per_page(20).send().await?;
 
-    let mut res = Vec::<String>::new();
+    let mut res = Vec::<(String, u32)>::new();
 
     for repo in repos_page {
         let url = repo.html_url.as_ref().map(|u| u.as_str().to_string());
+        let size_kb = repo.size.unwrap_or(0);
         if let Some(url) = url {
-            res.push(url);
+            res.push((url, size_kb));
         }
     }
 
@@ -21,14 +22,19 @@ async fn gather_repo_urls_from_user(
 }
 
 async fn clone_repos_into_dir(
-    repo_urls: Vec<String>,
+    repo_urls: Vec<(String, u32)>,
     target_dir: &PathBuf,
+    size_threshold_kb: u32,
 ) -> Result<Vec<GithubRepo>, git2::Error> {
     let mut res = Vec::<GithubRepo>::new();
-    for url in repo_urls {
-        let local_path = target_dir.join(&url);
-        let repo = GithubRepo::new_with_local_path(&*url, local_path.to_str().unwrap())?;
-        res.push(repo);
+    let mut total_cumulative_size: u32 = 0;
+    for (url, size) in repo_urls {
+        if total_cumulative_size + size < size_threshold_kb {
+            total_cumulative_size += size;
+            let local_path = target_dir.join(&url);
+            let repo = GithubRepo::new_with_local_path(&*url, local_path.to_str().unwrap())?;
+            res.push(repo);
+        }
     }
 
     Ok(res)
